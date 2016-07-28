b27de55b2670d566b9b9086bc61f29c8e1ec0029
John Tucker <john@larkintuckerllc.com>
2016-07-28 10:53:12 -0400

    start of screenshot

--- whiteboard/app.js
+++ whiteboard/app.js
@@ -5,17 +5,28 @@
   var thr0w = window.thr0w;
   document.addEventListener('DOMContentLoaded', ready);
   function ready() {
-    var grid;
+    var sync;
+    var cover = false;
     var active = true;
     var frameEl = document.getElementById('my_frame');
     var contentEl = document.getElementById('my_content');
+    var coverEl = document.getElementById('cover');
+    var captureEl = document.getElementById('capture');
+    var captureFrameLeftEl = document
+      .getElementById('capture__frame--left');
+    var captureFrameMiddleLeftEl = document
+      .getElementById('capture__frame--middle-left');
+    var captureFrameMiddleRightEl = document
+      .getElementById('capture__frame--middle-right');
+    var captureFrameRightEl = document
+      .getElementById('capture__frame--right');
     var base = window.location.protocol + '//' +
       window.location.hostname;
-    thr0w.setBase(base); // PROD
+    thr0w.setBase(base);
     thr0w.addAdminTools(frameEl,
       connectCallback, messageCallback);
     function connectCallback() {
-      grid = new thr0w.FlexGrid(
+      var grid = new thr0w.FlexGrid(
         frameEl,
         contentEl,
         [[6, 7, 8 , 9]],
@@ -27,16 +38,65 @@
           }
         ]
       );
+      sync = new thr0w.Sync(
+        grid,
+        'cover',
+        message,
+        receive
+      );
       thr0w.draw.load(grid);
       frameEl.addEventListener('touchstart', keepActive, true);
-      document.getElementById('back').addEventListener('click',
-        handleBackClick);
+      document.getElementById('controls__back').addEventListener('click',
+        handleControlsBackClick);
+      document.getElementById('controls__capture').addEventListener('click',
+        handleControlsCaptureClick);
       if (thr0w.getChannel() === 6) {
         document.getElementById('controls').style.display = 'block';
+        document.getElementById('capture__controls__cancel')
+          .addEventListener('click', handleCaptureControlsCancel);
       }
       window.setInterval(checkIdle, TIMEOUT);
+      function message() {
+        return {
+          cover: cover
+        };
+      }
+      function receive(data) {
+        window.console.log(data);
+        if (data.cover) {
+          coverEl.style.display = 'block';
+        } else {
+          coverEl.style.display = 'none';
+        }
+      }
     }
     function messageCallback(data) {
+      if (data.message.thr0w && data.message.thr0w.type === 'capture') {
+        switch (data.source) {
+          case 16:
+            cover = true;
+            captureFrameLeftEl.style.backgroundImage = 'url(' +
+              data.message.thr0w.dataUrl + ')';
+            coverEl.style.display = 'block';
+            captureEl.style.display = 'block';
+            sync.update();
+            break;
+          case 17:
+            captureFrameMiddleLeftEl.style.backgroundImage = 'url(' +
+              data.message.thr0w.dataUrl + ')';
+            break;
+          case 18:
+            captureFrameMiddleRightEl.style.backgroundImage = 'url(' +
+              data.message.thr0w.dataUrl + ')';
+            break;
+          case 19:
+            captureFrameRightEl.style.backgroundImage = 'url(' +
+              data.message.thr0w.dataUrl + ')';
+            break;
+          default:
+        }
+        return;
+      }
       if (data.message.type === 'idle') {
         document.location.href = '../';
       } else if (data.message.type === 'active') {
@@ -50,8 +110,24 @@
     function checkIdle() {
       keepActive();
     }
-    function handleBackClick() {
+    function handleControlsBackClick() {
       thr0w.thr0wChannel(CHANNELS, {type: 'idle'});
+    }
+    function handleControlsCaptureClick() {
+      thr0w.thr0w([16, 17, 18, 19], {
+        action: 'capture',
+        target: 6
+      });
+    }
+    function handleCaptureControlsCancel() {
+      cover = false;
+      captureFrameLeftEl.style.backgroundImage = 'none';
+      captureFrameMiddleLeftEl.style.backgroundImage = 'none';
+      captureFrameMiddleRightEl.style.backgroundImage = 'none';
+      captureFrameRightEl.style.backgroundImage = 'none';
+      captureEl.style.display = 'none';
+      coverEl.style.display = 'none';
+      sync.update();
     }
   }
 })();
